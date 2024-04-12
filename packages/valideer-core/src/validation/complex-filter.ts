7b6341f5cdbcfa99eb6a17b9a4c323c6fe4d9d11
Liam Ederzeel <liamederzeel@gmail.com>
2024-04-12 18:20:17 +0200

    feat: add enum support for complex filter

--- packages/valideer-core/src/validation/complex-filter.ts
+++ packages/valideer-core/src/validation/complex-filter.ts
@@ -4,111 +4,129 @@ import { parseIntList } from "./int-list";
 import { parseStringList } from "./string-list";
 import { parseDateList } from "./date-list";
 import { parseObjectIdList } from "./object-id-list";
+import { parseEnumList } from "./enum-list";
+import { IsNumberList } from ".";
 
 export const parseComplexFilter = (
-	value: Record<string, string | string[]> | string | string[],
-	subType: "int" | "boolean" | "string" | "date" | "objectId" = "int",
-	filter: "in" | "nin" | "gt" | "gte" | "lt" | "lte" | "eq" | "ne" = "in"
+  value: Record<string, string | string[]> | string | string[],
+  subType: "int" | "boolean" | "string" | "date" | "objectId" = "int",
+  filter: "in" | "nin" | "gt" | "gte" | "lt" | "lte" | "eq" | "ne" = "in",
+  options?: {
+    enum?: any;
+  },
 ): Record<string, any> => {
-	if (typeof value === "string" || Array.isArray(value)) {
-		switch (subType) {
-			case "int":
-				return { [`$${filter}`]: parseIntList(value) };
-			case "boolean":
-				return { [`$${filter}`]: parseBooleanList(value) };
-			case "string":
-				return { [`$${filter}`]: parseStringList(value) };
-			case "date":
-				return { [`$${filter}`]: parseDateList(value) };
-			case "objectId":
-				return { [`$${filter}`]: parseObjectIdList(value) };
-		}
-	}
+  if (typeof value === "string" || Array.isArray(value)) {
+    switch (subType) {
+      case "int":
+        return { [`$${filter}`]: parseIntList(value) };
+      case "boolean":
+        return { [`$${filter}`]: parseBooleanList(value) };
+      case "string":
+        if (options?.enum) {
+          return { [`$${filter}`]: parseEnumList(value, options.enum) };
+        }
+        return { [`$${filter}`]: parseStringList(value) };
+      case "date":
+        return { [`$${filter}`]: parseDateList(value) };
+      case "objectId":
+        return { [`$${filter}`]: parseObjectIdList(value) };
+    }
+  }
 
-	if (typeof value === "object") {
-		const filters = ["in", "nin", "gt", "gte", "lt", "lte", "eq", "ne"];
-		const filterObject = {} as Record<string, any>;
+  if (typeof value === "object") {
+    const filters = ["in", "nin", "gt", "gte", "lt", "lte", "eq", "ne"];
+    const filterObject = {} as Record<string, any>;
 
-		for (const filter of filters) {
-			if (value[filter] == null) continue;
+    for (const filter of filters) {
+      if (value[filter] == null) continue;
 
-			const valueList = parseStringList(value[filter]);
-			if (valueList.length > 0) {
-				const filterKey = `$${filter}`;
+      const valueList = parseStringList(value[filter]);
+      if (valueList.length > 0) {
+        const filterKey = `$${filter}`;
 
-				switch (subType) {
-					case "int":
-						filterObject[filterKey] = parseIntList(valueList);
-						break;
-					case "boolean":
-						filterObject[filterKey] = parseBooleanList(valueList);
-						break;
-					case "string":
-						filterObject[filterKey] = parseStringList(valueList);
-						break;
-					case "date":
-						filterObject[filterKey] = parseDateList(valueList);
-						break;
-					case "objectId":
-						filterObject[filterKey] = parseObjectIdList(valueList);
-						break;
-					default:
-						break;
-				}
-			}
-		}
+        switch (subType) {
+          case "int":
+            filterObject[filterKey] = parseIntList(valueList);
+            break;
+          case "boolean":
+            filterObject[filterKey] = parseBooleanList(valueList);
+            break;
+          case "string":
+            if (options?.enum) {
+              filterObject[filterKey] = parseEnumList(valueList, options.enum);
+            } else {
+              filterObject[filterKey] = parseStringList(valueList);
+            }
+            break;
+          case "date":
+            filterObject[filterKey] = parseDateList(valueList);
+            break;
+          case "objectId":
+            filterObject[filterKey] = parseObjectIdList(valueList);
+            break;
+          default:
+            break;
+        }
+      }
+    }
 
-		return filterObject;
-	}
+    return filterObject;
+  }
 
-	throw new Error("unexpected value");
+  throw new Error("unexpected value");
 };
 
-export const IsComplexFilter = (subType: "string" | "int" | "boolean" | "date" | "objectId") => {
-	return (object: Record<string, any>, propertyName: string): void => {
-		registerDecorator({
-			name: "isComplexFilter",
-			target: object.constructor,
-			propertyName,
-			constraints: [],
-			options: {
-				message: `${propertyName} is not a valid complex filter`,
-			},
-			validator: {
-				validate: (value: any) => {
-					if (value == null) return false;
+export const IsComplexFilter = (
+  subType: "string" | "int" | "boolean" | "date" | "objectId",
+) => {
+  return (object: Record<string, any>, propertyName: string): void => {
+    registerDecorator({
+      name: "isComplexFilter",
+      target: object.constructor,
+      propertyName,
+      constraints: [],
+      options: {
+        message: `${propertyName} is not a valid complex filter`,
+      },
+      validator: {
+        validate: (value: any) => {
+          if (value == null) return false;
 
-					const isRecord = (value: unknown): value is Record<string, string | string[]> => {
-						if (value == null) return false;
-						if (Array.isArray(value)) return false;
-						if (typeof value !== "object") return false;
-						return Object.values(value).every((x) => typeof x === "string" || isStringArray(x));
-					};
+          const isRecord = (
+            value: unknown,
+          ): value is Record<string, string | string[]> => {
+            if (value == null) return false;
+            if (Array.isArray(value)) return false;
+            if (typeof value !== "object") return false;
+            return Object.values(value).every(
+              (x) => typeof x === "string" || isStringArray(x),
+            );
+          };
 
-					const isString = (value: unknown): value is string => {
-						if (value == null) return false;
-						return typeof value === "string";
-					};
+          const isString = (value: unknown): value is string => {
+            if (value == null) return false;
+            return typeof value === "string";
+          };
 
-					const isStringArray = (value: unknown): value is string[] => {
-						if (value == null) return false;
-						if (!Array.isArray(value)) return false;
-						return value.every((x) => typeof x === "string");
-					};
+          const isStringArray = (value: unknown): value is string[] => {
+            if (value == null) return false;
+            if (!Array.isArray(value)) return false;
+            return value.every((x) => typeof x === "string");
+          };
 
-					if (isRecord(value) || isString(value) || isStringArray(value)) {
-						const parsed = parseComplexFilter(value, subType);
+          if (isRecord(value) || isString(value) || isStringArray(value)) {
+            const parsed = parseComplexFilter(value, subType);
 
-						if (Array.isArray(parsed)) {
-							return parsed.length > 0;
-						} else if (typeof parsed === "object") {
-							return Object.keys(parsed).length > 0;
-						}
-					}
+            if (Array.isArray(parsed)) {
+              return parsed.length > 0;
+            } else if (typeof parsed === "object") {
+              return Object.keys(parsed).length > 0;
+            }
+          }
 
-					return false;
-				},
-			},
-		});
-	};
+          return false;
+        },
+      },
+    });
+  };
 };
