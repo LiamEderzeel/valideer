2facf646126018b3313cbb648435930ed5244285
Liam Ederzeel <liamederzeel@gmail.com>
2024-08-01 12:26:24 +0200

    fix: ReqHandler export

--- packages/valideer-express/src/index.ts
+++ packages/valideer-express/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./validate-query.middleware";
 export * from "./validate-params.middleware";
 export * from "./validate-body.middleware";
+export * from "./req-handler";
 export * from "@valideer/core";
