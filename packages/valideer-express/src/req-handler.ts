1711a08f6a28a3f4aa77dcfb7e69b01ff49d9a9c
Liam Ederzeel <liamederzeel@gmail.com>
2025-10-16 12:04:33 +0200

    fix: express req handler

--- packages/valideer-express/src/req-handler.ts
+++ packages/valideer-express/src/req-handler.ts
@@ -1,17 +1,12 @@
+import { RequestHandler } from "express";
 import type core from "express-serve-static-core";
 
-export type Res<State extends Record<string, any>> = core.Response<any, State>;
+export type GlobalLocals = { [key: string]: any };
 
-export type Req<
-  ReqBody,
-  Body = Record<string, any>,
-  Query = core.Query,
-> = core.Request<core.ParamsDictionary, Body, ReqBody, Query>;
-
-export type DefaultState = {
-  [key: string]: any;
-};
-
-export type ReqHandler<State extends Record<string, any> = DefaultState> = {
-  (req: core.Request, res: Res<State>, next: core.NextFunction): void;
-};
+export type ReqHandler<
+  Locals = unknown,
+  Params extends core.ParamsDictionary = core.ParamsDictionary,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery extends core.Query = core.Query,
+> = RequestHandler<Params, ResBody, ReqBody, ReqQuery, GlobalLocals & Locals>;
