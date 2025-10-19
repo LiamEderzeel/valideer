9550d71e31263d0d987fe058c4447eb1c39141ad
Liam Ederzeel <liamederzeel@gmail.com>
2025-10-19 16:47:43 +0200

    ci: fix linting (#158)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import eslint from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -9,11 +8,16 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    rules: { "@typescript-eslint/no-explicit-any": "off" },
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 );
