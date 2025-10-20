77095268fc3c83188257eb39d43f085c3e311c15
Liam Ederzeel <liamederzeel@gmail.com>
2025-10-20 22:38:07 +0200

    fix: locales type

--- packages/valideer-core/src/validation/is-valid-locale.ts
+++ packages/valideer-core/src/validation/is-valid-locale.ts
@@ -1,197 +1,16 @@
-export const isValidLocale = (value: unknown): value is Extract<keyof ILanguages, string> => {
-	if (typeof value !== "string") return false;
+export type LocaleKey = (typeof locales)[number];
 
-	return locales.includes(value);
+export type ILanguages = {
+	[key in LocaleKey]?: string;
 };
 
-export declare interface ILanguages {
-	aa?: string; // Afar
-	ab?: string; // Abkhazian
-	ae?: string; // Avestan
-	af?: string; // Afrikaans
-	ak?: string; // Akan
-	am?: string; // Amharic
-	an?: string; // Aragonese
-	ar?: string; // Arabic
-	as?: string; // Assamese
-	av?: string; // Avaric
-	ay?: string; // Aymara
-	az?: string; // Azerbaijani
-	ba?: string; // Bashkir
-	be?: string; // Belarusian
-	bg?: string; // Bulgarian
-	bh?: string; // Bihari languages
-	bm?: string; // Bambara
-	bi?: string; // Bislama
-	bn?: string; // Bengali
-	bo?: string; // Tibetan
-	br?: string; // Breton
-	bs?: string; // Bosnian
-	ca?: string; // Catalan; Valencian
-	ce?: string; // Chechen
-	ch?: string; // Chamorro
-	co?: string; // Corsican
-	cr?: string; // Cree
-	cs?: string; // Czech
-	cu?: string; // Church Slavic; Old Slavonic; Church Slavonic; Old Bulgarian; Old Church Slavonic
-	cv?: string; // Chuvash
-	cy?: string; // Welsh
-	da?: string; // Danish
-	de?: string; // German
-	dv?: string; // Divehi; Dhivehi; Maldivian
-	dz?: string; // Dzongkha
-	ee?: string; // Ewe
-	el?: string; // Greek, Modern (1453-)
-	en?: string; // English
-	eo?: string; // Esperanto
-	es?: string; // Spanish; Castilian
-	et?: string; // Estonian
-	eu?: string; // Basque
-	fa?: string; // Persian
-	ff?: string; // Fulah
-	fi?: string; // Finnish
-	fj?: string; // Fijian
-	fo?: string; // Faroese
-	fr?: string; // French
-	fy?: string; // Western Frisian
-	ga?: string; // Irish
-	gd?: string; // Gaelic; Scottish Gaelic
-	gl?: string; // Galician
-	gn?: string; // Guarani
-	gu?: string; // Gujarati
-	gv?: string; // Manx
-	ha?: string; // Hausa
-	he?: string; // Hebrew
-	hi?: string; // Hindi
-	ho?: string; // Hiri Motu
-	hr?: string; // Croatian
-	ht?: string; // Haitian; Haitian Creole
-	hu?: string; // Hungarian
-	hy?: string; // Armenian
-	hz?: string; // Herero
-	ia?: string; // Interlingua (International Auxiliary Language Association)
-	id?: string; // Indonesian
-	ie?: string; // Interlingue; Occidental
-	ig?: string; // Igbo
-	ii?: string; // Sichuan Yi; Nuosu
-	ik?: string; // Inupiaq
-	io?: string; // Ido
-	is?: string; // Icelandic
-	it?: string; // Italian
-	iu?: string; // Inuktitut
-	ja?: string; // Japanese
-	jv?: string; // Javanese
-	ka?: string; // Georgian
-	kg?: string; // Kongo
-	ki?: string; // Kikuyu; Gikuyu
-	kj?: string; // Kuanyama; Kwanyama
-	kk?: string; // Kazakh
-	kl?: string; // Kalaallisut; Greenlandic
-	km?: string; // Central Khmer
-	kn?: string; // Kannada
-	ko?: string; // Korean
-	kr?: string; // Kanuri
-	ks?: string; // Kashmiri
-	ku?: string; // Kurdish
-	kv?: string; // Komi
-	kw?: string; // Cornish
-	ky?: string; // Kirghiz; Kyrgyz
-	la?: string; // Latin
-	lb?: string; // Luxembourgish; Letzeburgesch
-	lg?: string; // Ganda
-	li?: string; // Limburgan; Limburger; Limburgish
-	ln?: string; // Lingala
-	lo?: string; // Lao
-	lt?: string; // Lithuanian
-	lu?: string; // Luba-Katanga
-	lv?: string; // Latvian
-	mg?: string; // Malagasy
-	mh?: string; // Marshallese
-	mi?: string; // Maori
-	mk?: string; // Macedonian
-	ml?: string; // Malayalam
-	mn?: string; // Mongolian
-	mr?: string; // Marathi
-	ms?: string; // Malay
-	mt?: string; // Maltese
-	my?: string; // Burmese
-	na?: string; // Nauru
-	nb?: string; // Bokmål, Norwegian; Norwegian Bokmål
-	nd?: string; // Ndebele, North; North Ndebele
-	ne?: string; // Nepali
-	ng?: string; // Ndonga
-	nl?: string; // Dutch; Flemish
-	nn?: string; // Norwegian Nynorsk; Nynorsk, Norwegian
-	no?: string; // Norwegian
-	nr?: string; // Ndebele, South; South Ndebele
-	nv?: string; // Navajo; Navaho
-	ny?: string; // Chichewa; Chewa; Nyanja
-	oc?: string; // Occitan (post 1500)
-	oj?: string; // Ojibwa
-	om?: string; // Oromo
-	or?: string; // Oriya
-	os?: string; // Ossetian; Ossetic
-	pa?: string; // Panjabi; Punjabi
-	pi?: string; // Pali
-	pl?: string; // Polish
-	ps?: string; // Pushto; Pashto
-	pt?: string; // Portuguese
-	qu?: string; // Quechua
-	rm?: string; // Romansh
-	rn?: string; // Rundi
-	ro?: string; // Romanian; Moldavian; Moldovan
-	ru?: string; // Russian
-	rw?: string; // Kinyarwanda
-	sa?: string; // Sanskrit
-	sc?: string; // Sardinian
-	sd?: string; // Sindhi
-	se?: string; // Northern Sami
-	sg?: string; // Sango
-	si?: string; // Sinhala; Sinhalese
-	sk?: string; // Slovak
-	sl?: string; // Slovenian
-	sm?: string; // Samoan
-	sn?: string; // Shona
-	so?: string; // Somali
-	sq?: string; // Albanian
-	sr?: string; // Serbian
-	ss?: string; // Swati
-	st?: string; // Sotho, Southern
-	su?: string; // Sundanese
-	sv?: string; // Swedish
-	sw?: string; // Swahili
-	ta?: string; // Tamil
-	te?: string; // Telugu
-	tg?: string; // Tajik
-	th?: string; // Thai
-	ti?: string; // Tigrinya
-	tk?: string; // Turkmen
-	tl?: string; // Tagalog
-	tn?: string; // Tswana
-	to?: string; // Tonga (Tonga Islands)
-	tr?: string; // Turkish
-	ts?: string; // Tsonga
-	tt?: string; // Tatar
-	tw?: string; // Twi
-	ty?: string; // Tahitian
-	ug?: string; // Uighur; Uyghur
-	uk?: string; // Ukrainian
-	ur?: string; // Urdu
-	uz?: string; // Uzbek
-	ve?: string; // Venda
-	vi?: string; // Vietnamese
-	vo?: string; // Volapük
-	wa?: string; // Walloon
-	wo?: string; // Wolof
-	xh?: string; // Xhosa
-	yi?: string; // Yiddish
-	yo?: string; // Yoruba
-	za?: string; // Zhuang; Chuang
-	zh?: string; // Chinese
-	zu?: string; // Zulu
-}
+export const isValidLocale = (value: unknown): value is LocaleKey => {
+	if (typeof value !== "string") return false;
 
-export const locales: (Extract<keyof ILanguages, string> | string)[] = [
+	return (locales as readonly string[]).includes(value);
+};
+
+export const locales = [
 	"aa",
 	"ab",
 	"ae",
@@ -376,4 +195,4 @@ export const locales: (Extract<keyof ILanguages, string> | string)[] = [
 	"za",
 	"zh",
 	"zu",
-]
+] as const;
