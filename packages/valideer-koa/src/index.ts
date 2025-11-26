be7b61c762945c867e8cf7dfc5eec311643121b4
Liam Ederzeel <liamederzeel@gmail.com>
2025-11-26 14:08:43 +0100

    feat: export helper functions

--- packages/valideer-koa/src/index.ts
+++ packages/valideer-koa/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./validate-query.middleware";
+export * from "./validate-query";
 export * from "./validate-params.middleware";
+export * from "./validate-params";
 export * from "./validate-body.middleware";
+export * from "./validate-body";
 export * from "@valideer/core";
